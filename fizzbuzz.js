5c73c9af4bf077f4e39b0cbc817e25f9f44a1dd4
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-02-28 17:41:41 -0800

    Implemented fizzbuzz program

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz() {
+    for (var i = 1; i <= 100; i++) {
+        var output = "";
+        if (i % 3 === 0) {
+            output += "Fizz";
+        }
+        if (i % 5 === 0) {
+            output += "Buzz";
+        }
+        if (output === "") {
+            output = i.toString();
+        }
+        console.log(output);
+    }
+}
+fizzBuzz();
